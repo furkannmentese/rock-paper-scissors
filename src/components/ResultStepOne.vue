77be57dbc0f159f142108df8c0e9b1d6e99e3290
furkannmentese <furkannmentese@gmail.com>
2025-08-03 22:49:06 +0300

    ResulStepsOne component created

---
+++ src/components/ResultStepOne.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useGameStore } from '../stores/game.store'
+import { choices } from '../data/choices'
+
+const gameStore = useGameStore()
+
+const getChoiceData = (choiceName: string | null) =>
+  choices.find((choice) => choice.name === choiceName)
+
+const userChoiceImg = computed(() => getChoiceData(gameStore.userChoice)?.img || choices[0].img)
+const houseChoiceImg = computed(() => getChoiceData(gameStore.houseChoice)?.img || choices[0].img)
+</script>
+
+<template>
+  <section class="relative max-w-[700px] mx-auto grid grid-cols-2 gap-8 place-items-center pt-16">
+
+    <h2 class="text-2xl text-white uppercase font-bold font-display text-center pb-16">
+      YOU PICKED
+    </h2>
+    <h2 class="text-2xl text-white uppercase font-bold font-display text-center pb-16">
+      THE HOUSE PICKED
+    </h2>
+
+    <div
+      :class="[
+        gameStore.userChoice ? `choice-${gameStore.userChoice}` : '',
+        'relative',
+        'before:absolute before:-left-[15%] before:-top-[15%] before:w-[130%] before:h-[130%] before:rounded-full before:z-[1]',
+        'after:absolute after:-left-[15%] after:-top-[15%] after:w-[130%] after:h-[130%] after:rounded-full after:z-[1]',
+        'focus:after:shadow-[0_0_0_2rem_#223351] focus:after:z-0'
+      ]"
+    >
+      <div
+        class="choice relative w-60 h-60 bg-white rounded-full grid place-items-center shadow-[inset_0_0.5rem_#00000026] z-[2]"
+        :class="[gameStore.userChoice]"
+      >
+        <img 
+          :src="userChoiceImg"
+          :alt="gameStore.userChoice || ''"
+          class="scale-150 relative z-[1]"
+        />
+      </div>
+    </div>
+
+    <div
+      v-if="!gameStore.houseChoice"
+      class="relative before:absolute before:-left-[15%] before:-top-[15%] before:w-[130%] before:h-[130%] before:rounded-full before:bg-[#16213d]"
+    >
+      <div class="choice relative w-50 h-50"></div>
+    </div>
+
+    <div
+      v-else
+      :class="[
+        gameStore.houseChoice ? `choice-${gameStore.houseChoice}` : '',
+        'relative',
+        'before:absolute before:-left-[15%] before:-top-[15%] before:w-[130%] before:h-[130%] before:rounded-full before:z-[1]',
+        'after:absolute after:-left-[15%] after:-top-[15%] after:w-[130%] after:h-[130%] after:rounded-full after:z-[1]',
+        'focus:after:shadow-[0_0_0_2rem_#223351] focus:after:z-0'
+      ]"
+    >
+      <div
+        class="choice relative w-60 h-60 bg-white rounded-full grid place-items-center shadow-[inset_0_0.5rem_#00000026] z-[2]"
+        :class="[gameStore.houseChoice]"
+      >
+        <img 
+          :src="houseChoiceImg"
+          :alt="gameStore.houseChoice || ''"
+          class="scale-150 relative z-[1]"
+        />
+      </div>
+    </div>
+  </section>
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,12 +2,17 @@
 import Header from '../components/Header.vue'
 import Game from '../components/Game.vue'
 import Footer from '../components/Footer.vue'
+import ResultStepOne from '../components/ResultStepOne.vue'
+import { useGameStore } from '../stores/game.store'
+
+const gameStore = useGameStore()
 </script>
 
 <template>
   <div class="min-h-screen bg-gradient-radial relative overflow-hidden pb-32">
     <Header />
-    <Game />
+    <Game v-if="gameStore.gameState === 'choosing'" />
+    <ResultStepOne v-if="gameStore.gameState === 'playing' || gameStore.gameState === 'house-playing'" />
     <Footer />
   </div>
 </template>
