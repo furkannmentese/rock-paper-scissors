c1b4179f05c997d782f5245d37f3ba7541933aaa
furkannmentese <furkannmentese@gmail.com>
2025-08-03 19:54:54 +0300

    Game component created

---
+++ src/components/Game.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { choices } from '../data/choices'
+</script>
+
+<template>
+  <div class="relative max-w-[700px] mx-auto pt-8">
+    <section
+      class="game relative grid place-items-center h-[30rem] pt-16 before:absolute before:w-full before:h-full before:left-[28%] before:top-[35%] before:bg-[url('/images/bg-triangle.svg')] before:bg-no-repeat before:z-0"
+    >
+      <button
+        v-for="choice in choices"
+        :key="choice.name"
+        :class="[
+          'choice-btn',
+          `choice-${choice.name}`,
+          'relative',
+          'before:absolute before:-left-[15%] before:-top-[15%] before:w-[130%] before:h-[130%] before:rounded-full before:z-[1]',
+          'focus:after:content-[\'\'] focus:after:absolute focus:after:-left-[15%] focus:after:-top-[15%] focus:after:w-[130%] focus:after:h-[130%] focus:after:rounded-full focus:after:shadow-[0_0_0_2rem_#223351] focus:after:z-0',
+        ]"
+        :style="{ gridArea: choice.gridArea }"
+      >
+        <div
+          :class="[
+            'choice',
+            'relative w-40 h-40 bg-white rounded-full grid place-items-center shadow-[inset_0_0.5rem_#00000026] z-[2]',
+          ]"
+        >
+          <img :src="choice.img" :alt="choice.alt" class="scale-150 relative z-[1]" />
+        </div>
+      </button>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.game {
+  grid-template-areas:
+    'paper scissors'
+    'rock rock';
+}
+</style>
+
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Header from '../components/Header.vue'
+import Game from '../components/Game.vue'
 import Footer from '../components/Footer.vue'
 </script>
 
 <template>
   <div class="min-h-screen bg-gradient-radial relative overflow-hidden pb-32">
     <Header />
+    <Game />
     <Footer />
   </div>
 </template>
