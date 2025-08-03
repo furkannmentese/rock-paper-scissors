0121c50ebf9dae374110de6a7e3b368e9e49149b
furkannmentese <furkannmentese@gmail.com>
2025-08-03 19:54:08 +0300

    Removing colors data and adding them to tailwindcss

--- src/data/choices.ts
+++ src/data/choices.ts
@@ -3,10 +3,6 @@ export interface Choice {
   img: string
   alt: string
   gridArea: string
-  colors: {
-    background: string
-    shadow: string
-  }
 }
 
 export const choices: Choice[] = [
@@ -15,29 +11,17 @@ export const choices: Choice[] = [
     img: '/images/icon-paper.svg',
     alt: 'Paper',
     gridArea: 'paper',
-    colors: {
-      background: '#4664f4',
-      shadow: '#2a45c2',
-    },
   },
   {
     name: 'scissors',
     img: '/images/icon-scissors.svg',
     alt: 'Scissors',
     gridArea: 'scissors',
-    colors: {
-      background: '#eb9f0e',
-      shadow: '#c76c1b',
-    },
   },
   {
     name: 'rock',
     img: '/images/icon-rock.svg',
     alt: 'Rock',
     gridArea: 'rock',
-    colors: {
-      background: '#db2e4d',
-      shadow: '#9d1634',
-    },
   },
 ]
